ff82f0e646a03df9f478a06adecc735774cf33dd
YuanfnegGu <yuanfenggu@hotmail.com>
2020-08-29 21:40:51 -0500

    Models update

--- myExpenseReimbursementSystem/src/main/java/com/revature/models/Reimbursement.java
+++ myExpenseReimbursementSystem/src/main/java/com/revature/models/Reimbursement.java
@@ -1,4 +1,145 @@
 package com.revature.models;
 
+import java.sql.Timestamp;
+import java.util.Objects;
+
 public class Reimbursement {
+
+    //fields
+
+    private int reimbId;
+    private double amount;
+    private String description;
+    private int authorId;
+    private int resolverId;
+    private int statusId;
+    private int typeId;
+    private Timestamp dateSubmitted;
+    private Timestamp resolvedDate;
+
+    //constructor
+
+    public Reimbursement() {
+    }
+
+    public Reimbursement(int reimbId, double amount, Timestamp dateSubmitted,Timestamp resolvedDate,String description,
+                         int authorId, int resolverId, int statusId, int typeId) {
+        this.reimbId = reimbId;
+        this.amount = amount;
+        this.description = description;
+        this.authorId = authorId;
+        this.resolverId = resolverId;
+        this.statusId = statusId;
+        this.typeId = typeId;
+        this.dateSubmitted = dateSubmitted;
+        this.resolvedDate = resolvedDate;
+    }
+
+    public int getReimbId() {
+        return reimbId;
+    }
+
+    public void setReimbId(int reimbId) {
+        this.reimbId = reimbId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public int getResolverId() {
+        return resolverId;
+    }
+
+    public void setResolverId(int resolverId) {
+        this.resolverId = resolverId;
+    }
+
+    public int getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(int statusId) {
+        this.statusId = statusId;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public Timestamp getDateSubmitted() {
+        return dateSubmitted;
+    }
+
+    public void setDateSubmitted(Timestamp dateSubmitted) {
+        this.dateSubmitted = dateSubmitted;
+    }
+
+    public Timestamp getResolvedDate() {
+        return resolvedDate;
+    }
+
+    public void setResolvedDate(Timestamp resolvedDate) {
+        this.resolvedDate = resolvedDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Reimbursement)) return false;
+        Reimbursement that = (Reimbursement) o;
+        return getReimbId() == that.getReimbId() &&
+                Double.compare(that.getAmount(), getAmount()) == 0 &&
+                getAuthorId() == that.getAuthorId() &&
+                getResolverId() == that.getResolverId() &&
+                getStatusId() == that.getStatusId() &&
+                getTypeId() == that.getTypeId() &&
+                Objects.equals(getDescription(), that.getDescription()) &&
+                Objects.equals(getDateSubmitted(), that.getDateSubmitted()) &&
+                Objects.equals(getResolvedDate(), that.getResolvedDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getReimbId(), getAmount(), getDescription(), getAuthorId(), getResolverId(), getStatusId(), getTypeId(), getDateSubmitted(), getResolvedDate());
+    }
+
+    @Override
+    public String toString() {
+        return "Reimbursement{" +
+                "reimbId=" + reimbId +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                ", authorId=" + authorId +
+                ", resolverId=" + resolverId +
+                ", statusId=" + statusId +
+                ", typeId=" + typeId +
+                ", dateSubmitted=" + dateSubmitted +
+                ", resolvedDate=" + resolvedDate +
+                '}';
+    }
 }
--- myExpenseReimbursementSystem/src/main/java/com/revature/models/User.java
+++ myExpenseReimbursementSystem/src/main/java/com/revature/models/User.java
@@ -1,4 +1,119 @@
 package com.revature.models;
 
+import java.util.Objects;
+
 public class User {
+
+    // fields
+    private int id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int role;
+
+    //constructor
+
+
+    public User() {
+    }
+
+    public User(int id, String username, String password, String firstName, String lastName, String email, int role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return getId() == user.getId() &&
+                getRole() == user.getRole() &&
+                Objects.equals(getUsername(), user.getUsername()) &&
+                Objects.equals(getPassword(), user.getPassword()) &&
+                Objects.equals(getFirstName(), user.getFirstName()) &&
+                Objects.equals(getLastName(), user.getLastName()) &&
+                Objects.equals(getEmail(), user.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getUsername(), getPassword(), getFirstName(), getLastName(), getEmail(), getRole());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                '}';
+    }
 }
